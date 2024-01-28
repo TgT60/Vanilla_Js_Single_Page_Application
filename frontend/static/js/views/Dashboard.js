3b9440378ed369bb6afc36e7b02411c7c3676b11
TgT60 <dima.icigo@yandex.ru>
2024-01-28 17:11:37 +0300

    Made simple Vanilla JS SPA Application

---
+++ frontend/static/js/views/Dashboard.js
@@ -0,0 +1,20 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView{
+    constructor() {
+        super();
+        this.setTitle("Dashboard")
+    }
+
+    async getHtml() {
+        return `
+            <h1> Welcome back, dom</h1>
+            <p>
+                Here info about My First Single Page Application
+            </p>
+            <p>
+                <a href="/posts" data-link> View recent posts</a>
+            </p>
+        `;
+    }
+}
